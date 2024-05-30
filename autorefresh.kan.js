9f77b43a9b7eab6cc1717fdcc99e30b5508d2ff5
ckanvin <67659445+ckanvin@users.noreply.github.com>
2024-05-30 10:43:28 +0800

    Update autorefresh.kan.js

--- autorefresh.kan.js
+++ autorefresh.kan.js
@@ -1,15 +1,15 @@
 // ==UserScript==
 // @name         Auto Refresh Script
 // @namespace    http://tampermonkey.net/
-// @version      0.1.1
+// @version      0.1.3
 // @description  try to take over the world!
 // @author       You
 // @match        https://ng4a.gmcc.net/iga/index.html
 // @match        http://eip.gmcc.net/*
 // @match        http://oa.gmcc.net/*
 // @grant        none
-// @updateURL    https://github.com/ckanvin/kanvin-autorefresh/blob/main/autorefresh.kan.js
-// @downloadURL  https://github.com/ckanvin/kanvin-autorefresh/blob/main/autorefresh.kan.js
+// @updateURL    https://raw.githubusercontent.com/ckanvin/kanvin-autorefresh/main/autorefresh.kan.js
+// @downloadURL  https://raw.githubusercontent.com/ckanvin/kanvin-autorefresh/main/autorefresh.kan.js
 // @run-at       document-idle
 // ==/UserScript==
 
